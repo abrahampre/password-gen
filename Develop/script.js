054b9b819a8775dad27efb44063474ec729015f8
abraham perez reyes <abrahampre27@gmail.com>
2021-07-17 18:21:21 -0500

    password working now

--- Develop/script.js
+++ Develop/script.js
@@ -1,46 +1,39 @@
 
-// Seleccionando el boton generar password,
-// lo mandamos al final con el event listener para 
-//el momento del click
+
 var generateBtn = document.querySelector("#generate");
-var passwordOptionsSelected2 = "";
+
 
 
 function confirmData (){
 
+  var passwordOptionsSelected2 = [];
   var upperCaseConfirm = window.confirm ("Do you want uppercase in your passowrd?");
     if (upperCaseConfirm){
      var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+     passwordOptionsSelected2 = passwordOptionsSelected2.concat(upperCaseConfirm);
     };
   var lowerCaseConfirm = window.confirm ("Do you want lowercase in your passowrd?");
     if (lowerCaseConfirm){
       var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+     passwordOptionsSelected2 = passwordOptionsSelected2.concat(lowerCase);
     };
   var numericConfirm = window.confirm("Do you want numers in your password?");
     if (numericConfirm){
      var numeric = ['0','1','2','3','4','5','6','7','8','9'];
+     passwordOptionsSelected2 = passwordOptionsSelected2.concat(numeric);
     };
   var specialCharConfirm = window.confirm("Do you want special characters in your password");
     if (specialCharConfirm){
      var specialChar = ['<','(','[','{','\\','^','-','=','$','!','|',']','}',')','?','*','+','.','>'];
+     passwordOptionsSelected2 = passwordOptionsSelected2.concat(specialChar);
     };
 
   while(upperCase ===false  && lowerCase ===false && numeric ===false && specialChar === false){
     window.alert("Please select at least one criteria!");
     confirmData();
   };
-  // console.log(upperCase);
-  // console.log(lowerCase);
-  // console.log(numeric);
-  // console.log(specialChar);
+  console.log(passwordOptionsSelected2);
 
-   var passwordOptionsSelected = upperCaseConfirm.toString () + lowerCaseConfirm.toString() + numericConfirm.toString() + specialCharConfirm.toString() ;
-   console.log(passwordOptionsSelected);
-
-   passwordOptionsSelected2 = [upperCase + lowerCase + numeric + specialChar] ;
-   console.log(passwordOptionsSelected2);
-
-   console.log(passwordOptionsSelected2.length);
 
   };
 
@@ -64,26 +57,25 @@ function writePassword() {
   ////como crear un string with the for loop?????? preguntar a TA
     for(var i=0; i<lengthOfPassword; i++){
    // console.log("Array of total selected options", passwordOptionsSelected2);
-   
-   var randomNumber= (Math.floor(Math.random()*lengthOfPassword));
+  randomNumber= (Math.floor(Math.random()*lengthOfPassword)); //<---this one working
+  passwordOptionsSelected2 [randomNumber];
+  console.log(passwordOptionsSelected2[randomNumber])
+  generatePassword += passwordOptionsSelected2[randomNumber]; //<---this one doesnt work error not a function
+  console.log(generatePassword);
+  //console.log ("this is the generated password" ,generatePassword);
+
+
+
+   //este es el bueno -->>var randomNumber= (Math.floor(Math.random()*lengthOfPassword));
    console.log ("random number", randomNumber);
    
-   //generatePassword[i]=
-   
-   //generatePassword += random
-   
-   //passwordOptionsSelected2 [randomNumber]
-    //char[] text = new char[lengthOfPassword]
-    
-    //generatePassword += passwordOptionsSelected2[Math.floor(Math.random()*lengthOfPassword)];
-   // console.log ("picked random matching length",generatePassword);
     };
-    system.out.println() 
+  
 
-  var password = generatePassword();
+  //var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = generatePassword;
 
 }
 
