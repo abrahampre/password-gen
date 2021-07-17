d7a1636327bc1ac62b1bb791ba52400a2f4bfb7c
abraham perez reyes <abrahampre27@gmail.com>
2021-07-16 22:09:02 -0500

    added all the questions and return if non selected

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,41 @@
-// Assignment code here
-var generatePassword 
-
-//Distintas variables
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var numeric = ['0','1','2','3','4','5','6','7','8','9'];
-var specialChar = ['<','(','[','{','\\','^','-','=','$','!','|',']','}',')','?','*','+','.','>'];
 
 // Seleccionando el boton generar password,
 // lo mandamos al final con el event listener para 
 //el momento del click
 var generateBtn = document.querySelector("#generate");
+
+function confirmData (){
+
+  var upperCase = window.confirm ("Do you want uppercase in your passowrd?");
+    if (upperCase){
+      upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    };
+  var lowerCase = window.confirm ("Do you want lowercase in your passowrd?");
+    if (lowerCase){
+      lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    };
+  var numeric = window.confirm("Do you want numers in your password?");
+    if (numeric){
+      numeric = numeric = ['0','1','2','3','4','5','6','7','8','9'];
+    };
+  var specialChar = window.confirm("Do you want special characters in your password");
+    if (specialChar){
+      specialChar = ['<','(','[','{','\\','^','-','=','$','!','|',']','}',')','?','*','+','.','>'];
+    }
+
+  while(upperCase ===false  && lowerCase ===false && numeric ===false && specialChar === false){
+    window.prompt("Please select at least one criteria!");
+    confirmData();
+  };
+  console.log(upperCase);
+  console.log(lowerCase);
+  console.log(numeric);
+  console.log(specialChar);
+
+   var passwordOptionsSelected = [upperCase + lowerCase + numeric + specialChar ];
+   console.log(passwordOptionsSelected);
+
+};
 
 // Write password to the #password input
 function writePassword() {
@@ -20,11 +45,16 @@ function writePassword() {
   if (length < 8 || length> 128){
     alert ("Number not valid. Please select again");
     writePassword ()
-  }
+  };
     
+  confirmData();
   
+    for(i=0; i<length; i++){
+    var generatePassword= passwordOptionsSelected[Math.floor(Math.random()*length)]
+    console.log (generatePassword);
+    }
 
-  
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
