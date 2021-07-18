ab601eb1d0f5b5045bd0785edeeaab0cae6c21e8
abraham perez reyes <abrahampre27@gmail.com>
2021-07-17 21:26:46 -0500

    final, done with the code

--- Develop/script.js
+++ Develop/script.js
@@ -8,7 +8,7 @@ var passwordOptionsSelected2 = [];
 
 function confirmData (){
   
- var upperCaseConfirm = window.confirm ("Do you want uppercase in your password?");
+  var upperCaseConfirm = window.confirm ("Do you want uppercase in your password?");
     if (upperCaseConfirm){
      var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
      passwordOptionsSelected2 =  passwordOptionsSelected2.concat(upperCase);
@@ -33,46 +33,43 @@ function confirmData (){
      console.log(passwordOptionsSelected2);
     };
 
-  if (upperCaseConfirm ===false && lowerCaseConfirm ===false && numericConfirm ===false && specialCharConfirm === false){
-    window.alert("Please select at least one criteria!");
-    confirmData();
-  };
+    if (upperCaseConfirm ===false && lowerCaseConfirm ===false && numericConfirm ===false && specialCharConfirm === false){
+      window.alert("Please select at least one criteria!");
+      confirmData();
+    };
   console.log(passwordOptionsSelected2);
-
-
 };
 
-// Write password to the #password input
+
 function writePassword() {
 
-  alert("Welcome to the password generator, please select the following criteria!")
+  alert("Welcome to the password generator, please select the following criteria!");
   var lengthOfPassword = window.prompt ("How many character do you want on your password? Please select a number not less than 8 or grater than 128");
   if (lengthOfPassword < 8 || lengthOfPassword> 128){
     alert ("Number not valid. Please select again");
     writePassword ()
   };
- 
+ //insert teh confirm data
   confirmData();
+
+ //take the length of the confirm data
   console.log("from here we are going to take the length",passwordOptionsSelected2);
 
   var lengthOfOptions = passwordOptionsSelected2.length;
-  console.log(passwordOptionsSelected2.length);
+ 
   var generatePassword = "";
-  ////como crear un string with the for loop?????? preguntar a TA
+    ////como crear un string with the for loop?????? preguntar a TA
     for(var i=0; i<lengthOfPassword; i++){
-   // console.log("Array of total selected options", passwordOptionsSelected2);
-  randomNumber= (Math.floor(Math.random()*lengthOfOptions)); //<---this one working
-  
-  
-   console.log ("random number", randomNumber);
+ 
+      randomNumber= (Math.floor(Math.random()*lengthOfOptions)); //<---this one working
+      console.log ("random number", randomNumber);
  
   
-  passwordOptionsSelected2[randomNumber];
-  //console.log(passwordOptionsSelected2)
-
-  console.log(passwordOptionsSelected2[randomNumber])
-  generatePassword += passwordOptionsSelected2[randomNumber]; //<---this one doesnt work error not a function
-  console.log("this is the generated password",generatePassword);
+      passwordOptionsSelected2[randomNumber];
+      console.log(passwordOptionsSelected2[randomNumber])
+  
+      generatePassword += passwordOptionsSelected2[randomNumber]; 
+      console.log("this is the generated password",generatePassword);
    
     };
   
@@ -85,7 +82,7 @@ function writePassword() {
 }
 
 // Le agregamos un event listener al la variable creada
-//que selecciona con query selector el boton... despues
+// que selecciona con query selector el boton... despues
 // llamamos al writePassword function.
 generateBtn.addEventListener("click", writePassword);
 
