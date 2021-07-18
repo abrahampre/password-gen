791b210f58d350c0adc3ebfd1aa9f18f7d7eb2ae
abraham perez reyes <abrahampre27@gmail.com>
2021-07-18 11:40:56 -0500

    adding comments to code

--- Develop/script.js
+++ Develop/script.js
@@ -7,16 +7,20 @@ var passwordOptionsSelected2 = [];
 //Function to pick the options with a window confirm
 
 function confirmData (){
-  
+  //uppercase confim
   var upperCaseConfirm = window.confirm ("Do you want uppercase in your password?");
+    //introducing upper case if selected
     if (upperCaseConfirm){
      var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+     //introducing if positive
      passwordOptionsSelected2 =  passwordOptionsSelected2.concat(upperCase);
      console.log(passwordOptionsSelected2);
     };
+     //same steps with all the options required
   var lowerCaseConfirm = window.confirm ("Do you want lowercase in your password?");
     if (lowerCaseConfirm){
-      var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+     var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+     
      passwordOptionsSelected2 = passwordOptionsSelected2.concat(lowerCase);
      console.log(passwordOptionsSelected2);
     };
@@ -32,7 +36,7 @@ function confirmData (){
      passwordOptionsSelected2 = passwordOptionsSelected2.concat(specialChar);
      console.log(passwordOptionsSelected2);
     };
-
+    //return to formula if non of the options is selected
     if (upperCaseConfirm ===false && lowerCaseConfirm ===false && numericConfirm ===false && specialCharConfirm === false){
       window.alert("Please select at least one criteria!");
       confirmData();
@@ -40,34 +44,34 @@ function confirmData (){
   console.log(passwordOptionsSelected2);
 };
 
-
+//start of the function
 function writePassword() {
-
+  //requesting length of password from user
   alert("Welcome to the password generator, please select the following criteria!");
   var lengthOfPassword = window.prompt ("How many character do you want on your password? Please select a number not less than 8 or grater than 128");
   if (lengthOfPassword < 8 || lengthOfPassword> 128){
     alert ("Number not valid. Please select again");
     writePassword ()
   };
- //insert teh confirm data
+ //insert the confirm data function that is written above
   confirmData();
 
  //take the length of the confirm data
   console.log("from here we are going to take the length",passwordOptionsSelected2);
-
+ //creating number to use th elength of options selected on confirmData function
   var lengthOfOptions = passwordOptionsSelected2.length;
  
   var generatePassword = "";
-    ////como crear un string with the for loop?????? preguntar a TA
+    ////foor loop to get the random selected from the options picked on confirmData()
     for(var i=0; i<lengthOfPassword; i++){
- 
+      //Creating a random number from the length options
       randomNumber= (Math.floor(Math.random()*lengthOfOptions)); //<---this one working
+      
       console.log ("random number", randomNumber);
- 
-  
+      //picking the options randomly with the random number
       passwordOptionsSelected2[randomNumber];
       console.log(passwordOptionsSelected2[randomNumber])
-  
+      //passing the value to the generate password to then send it to the page  
       generatePassword += passwordOptionsSelected2[randomNumber]; 
       console.log("this is the generated password",generatePassword);
    
@@ -76,13 +80,10 @@ function writePassword() {
 
  
   var passwordText = document.querySelector("#password");
-
+  //passing the password generated from the for lool into the webpage.
   passwordText.value = generatePassword;
 
 }
 
-// Le agregamos un event listener al la variable creada
-// que selecciona con query selector el boton... despues
-// llamamos al writePassword function.
 generateBtn.addEventListener("click", writePassword);
 
